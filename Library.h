a5f59aafc96e724b56ec8be15c909b3a6d4b93cb
Watersheep94 <watersheep94@berkeley.edu>
2014-01-22 19:36:33 -0800

    Create Library.h

---
+++ Library.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include "Weapon.h"
+#include "Armor.h"
+#include "Character.h"
+#include "Enemy.h"
+#include "Globals.h"
+
+using namespace std;
